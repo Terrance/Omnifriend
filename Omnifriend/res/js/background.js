1e316eeff426139722b8951d5aa59ca6f1403dae
Ollie Terrance <ollie.terrance@live.co.uk>
2014-08-06 21:16:54 +0100

    Add Twitter following sync

--- Omnifriend/res/js/background.js
+++ Omnifriend/res/js/background.js
@@ -1,8 +1,21 @@
+chrome.runtime.onInstalled.addListener(function (object) {
+    chrome.tabs.create({url: chrome.runtime.getURL("/res/html/options.html")});
+});
 var friends;
 chrome.omnibox.onInputStarted.addListener(function() {
     friends = [];
     chrome.storage.local.get(function(store) {
-        if (store["fb-friends"]) friends = friends.concat(store["fb-friends"]);
+        var srcs = {
+            "fb": "Facebook",
+            "tw": "Twitter"
+        };
+        ["fb-friends", "tw-follows"].map(function(key, i, arr) {
+            if (store[key]) {
+                var src = srcs[key.substr(0, 2)];
+                for (var j in store[key]) store[key][j].src = src;
+                friends = friends.concat(store[key]);
+            }
+        });
     });
 });
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
@@ -14,7 +27,7 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
             var desc = friends[i].name.substr(0, index)
                      + "<match>" + friends[i].name.substr(index, text.length) + "</match>"
                      + friends[i].name.substr(index + text.length)
-                     + "  <url>Facebook</url>";
+                     + "  <url>" + friends[i].src + "</url>";
             var match = {
                 content: friends[i].url,
                 description: desc.replace(/&/g, "&amp;")
--- Omnifriend/res/js/options.js
+++ Omnifriend/res/js/options.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
                         });
                         chrome.storage.local.set({"fb-friends": friends}, function() {
                             $("#fb-perms, #fb-sync").prop("disabled", false);
-                            $("#fb-status").removeClass("alert-warning").addClass("alert-success").text(resp.payload.entries.length + " friends saved.");
+                            $("#fb-status").removeClass("alert-warning").addClass("alert-success").text(friends.length + " friends saved.");
                             $("#fb-clear").prop("disabled", false);
                         });
                     },
@@ -81,6 +81,105 @@ $(document).ready(function() {
                 $("#fb-clear").prop("disabled", true);
                 chrome.storage.local.remove("fb-friends", function() {
                     $("#fb-status").removeClass("alert-danger").addClass("alert-info").text("Press \"Sync\" to update from Facebook.");
+                })
+            }
+        });
+        chrome.permissions.contains({
+            origins: ["https://twitter.com/", "https://mobile.twitter.com/"]
+        }, function(has) {
+            if (has) {
+                $("#tw-perms").addClass("btn-success").find("span").text("Enabled");
+                if (store["tw-follows"] && store["tw-follows"].length) {
+                    $("#tw-status").addClass("alert-success").text(store["tw-follows"].length + " follows saved.");
+                } else {
+                    $("#tw-status").addClass("alert-info").text("Press \"Sync\" to update from Twitter.");
+                    $("#tw-clear").prop("disabled", true);
+                }
+            } else {
+                $("#tw-perms").addClass("btn-danger").find("span").text("Disabled");
+                $("#tw-sync").prop("disabled", true);
+                $("#tw-status").addClass("alert-danger").text("No permissions to get Twitter data.");
+            }
+        });
+        $("#tw-perms").click(function(e) {
+            if ($("#tw-perms").hasClass("btn-danger")) {
+                chrome.permissions.request({
+                    origins: ["https://twitter.com/", "https://mobile.twitter.com/"]
+                }, function(success) {
+                    if (success) {
+                        $("#tw-perms").removeClass("btn-danger").addClass("btn-success").find("span").text("Enabled");
+                        $("#tw-sync").prop("disabled", false);
+                        $("#tw-status").removeClass("alert-danger").addClass("alert-info").text("Press \"Sync\" to update from Twitter.");
+                    }
+                });
+            } else {
+                chrome.permissions.remove({
+                    origins: ["https://twitter.com/", "https://mobile.twitter.com/"]
+                }, function(success) {
+                    if (success) {
+                        $("#tw-perms").removeClass("btn-success").addClass("btn-danger").find("span").text("Disabled");
+                        $("#tw-sync").prop("disabled", true);
+                        $("#tw-status").removeClass("alert-info").addClass("alert-danger").text("No permissions to get Twitter data.");
+                    }
+                });
+            }
+        });
+        $("#tw-sync").click(function(e) {
+            $("#tw-perms, #tw-sync").prop("disabled", true);
+            $("#tw-status").removeClass("alert-info alert-danger alert-success").addClass("alert-warning").text("Fetching from Twitter...");
+            $.ajax({
+                // mobile site loads much faster than desktop
+                url: "https://mobile.twitter.com/settings",
+                success: function(resp, stat, xhr) {
+                    var username = $(".setting-value", resp)[14];
+                    if (username) {
+                        username = $(username).text().trim();
+                        $("#tw-status").text("Fetching followers for " + username + "...");
+                        var follows = [];
+                        function iter(cursor) {
+                            $.ajax({
+                                url: "https://twitter.com/" + username + "/following/users" + (cursor ? "?cursor=" + cursor : ""),
+                                success: function(resp, stat, xhr) {
+                                    $(".ProfileNameTruncated-link", resp.items_html).each(function(i, follow) {
+                                        var user = follow.href.split("/").pop();
+                                        follows.push({
+                                            name: follow.text.trim() + " (@" + user + ")",
+                                            url: "https://twitter.com/" + user
+                                        });
+                                    });
+                                    if (resp.cursor === "0") {
+                                        chrome.storage.local.set({"tw-follows": follows}, function() {
+                                            $("#tw-perms, #tw-sync").prop("disabled", false);
+                                            $("#tw-status").removeClass("alert-warning").addClass("alert-success").text(follows.length + " follows saved.");
+                                            $("#tw-clear").prop("disabled", false);
+                                        });
+                                    } else {
+                                        iter(resp.cursor);
+                                        $("#tw-status").text("Fetching followers for " + username + "... (" + follows.length + " so far)");
+                                    }
+                                },
+                                error: function(xhr, stat, err) {
+                                    console.err(arguments);
+                                }
+                            });
+                        }
+                        iter();
+                    } else {
+                        $("#tw-perms, #tw-sync").prop("disabled", false);
+                        $("#tw-status").removeClass("alert-warning").addClass("alert-danger").text("Failed to get username, are you logged in?");
+                    }
+                },
+                error: function(xhr, stat, err) {
+                    $("#tw-perms, #tw-sync").prop("disabled", false);
+                    $("#tw-status").removeClass("alert-warning").addClass("alert-danger").text("Failed to get username, are you logged in?");
+                }
+            });
+        });
+        $("#tw-clear").click(function(e) {
+            if (confirm("Remove all cached Twitter follows?")) {
+                $("#tw-clear").prop("disabled", true);
+                chrome.storage.local.remove("tw-follows", function() {
+                    $("#tw-status").removeClass("alert-danger").addClass("alert-info").text("Press \"Sync\" to update from Twitter.");
                 })
             }
         });
